308313ca4e434e8786d2a4e39d7514ac476173ac
koosha <k.e.khorasani@gmail.com>
2023-08-25 06:45:28 -0500

    SmartContracts are uploaded

---
+++ smartcontracts/managment-contract/managmentContract.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/hyperledger/fabric-contract-api-go/contractapi"
+	"log"
+	"managment-contract/chaincode"
+)
+
+func main() {
+	assetChaincode, err := contractapi.NewChaincode(&chaincode.SmartContract{})
+	if err != nil {
+		log.Panicf("Error creating managment chaincode: %v", err)
+	}
+
+	if err := assetChaincode.Start(); err != nil {
+		log.Panicf("Error starting managment chaincode: %v", err)
+	}
+}
