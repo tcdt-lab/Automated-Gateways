ccf2c42e41e24c3ee27fea119493e581cd2c8bac
koosha <k.e.khorasani@gmail.com>
2023-09-06 23:35:38 -0500

    Server is finished. Test is in progress
    Signed-off-by: koosha <k.e.khorasani@gmail.com>

--- relay/cmd/main.go
+++ relay/cmd/main.go
@@ -1,7 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"log"
+	"net"
+	"relay/scripts"
+	"relay/src/offchain"
+)
+
+var (
+	tls        = flag.Bool("tls", false, "Connection uses TLS if true, else plain TCP")
+	certFile   = flag.String("cert_file", "", "The TLS cert file")
+	keyFile    = flag.String("key_file", "", "The TLS key file")
+	jsonDBFile = flag.String("json_db_file", "", "A json file containing a list of features")
+	port       = flag.Int("port", 50051, "The server port")
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	startServer()
+}
+
+func startServer() {
+	log.Println("gRPC server is starting...")
+	flag.Parse()
+	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	var opts []grpc.ServerOption
+	if *tls {
+		if *certFile == "" {
+			//*certFile = data.Path("x509/server_cert.pem")
+		}
+		if *keyFile == "" {
+			//*keyFile = data.Path("x509/server_key.pem")
+		}
+		creds, err := credentials.NewServerTLSFromFile(*certFile, *keyFile)
+		if err != nil {
+			log.Fatalf("Failed to generate credentials: %v", err)
+		}
+		opts = []grpc.ServerOption{grpc.Creds(creds)}
+	}
+	grpcServer := grpc.NewServer(opts...)
+	s := offchain.IOPserver{}
+	scripts.RegisterIOPServer(grpcServer, &s)
+	grpcServer.Serve(lis)
 }
