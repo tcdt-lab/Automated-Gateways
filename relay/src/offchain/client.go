78c01d09fe0c92bdd6aab614bd4d19ff7afef490
koosha <k.e.khorasani@gmail.com>
2023-09-20 01:55:39 -0500

    Cert Authentication is added
    Signed-off-by: koosha <k.e.khorasani@gmail.com>

--- relay/src/offchain/client.go
+++ relay/src/offchain/client.go
@@ -13,13 +13,13 @@ import (
 )
 
 var (
-	tls                = flag.Bool("tls", false, "Connection uses TLS if true, else plain TCP")
+	//tls                = flag.Bool("tls", false, "Connection uses TLS if true, else plain TCP")
 	caFile             = flag.String("ca_file", "", "The file containing the CA root cert file")
 	serverAddr         = flag.String("addr", "localhost:50051", "The server address in the format of host:port")
 	serverHostOverride = flag.String("server_host_override", "x.test.example.com", "The server name used to verify the hostname returned by the TLS handshake")
 )
 
-func main() {
+func RunClient() {
 	flag.Parse()
 	var opts []grpc.DialOption
 	opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
