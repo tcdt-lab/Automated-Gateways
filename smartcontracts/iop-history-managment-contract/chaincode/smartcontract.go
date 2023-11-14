3cc60138186252ab10a51407c632cd5bdb2c2c19
koosha <k.e.khorasani@gmail.com>
2023-11-13 23:39:39 -0600

    Continue with IOP
    Signed-off-by: koosha <k.e.khorasani@gmail.com>

--- smartcontracts/iop-history-managment-contract/chaincode/smartcontract.go
+++ smartcontracts/iop-history-managment-contract/chaincode/smartcontract.go
@@ -2,6 +2,9 @@ package chaincode
 
 import "github.com/hyperledger/fabric-contract-api-go/contractapi"
 
+const PermittedMethodIdPrefix = "Iop_History"
+const insideInitiatedCallPrefix = "Inside_initiated_call"
+
 type SmartContract struct {
 	contractapi.Contract
 }
