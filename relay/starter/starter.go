663b8881e39de065a439f1821edfb2e193b703c4
koosha <k.e.khorasani@gmail.com>
2023-12-03 19:32:21 -0600

    starter is added

--- relay/starter/starter.go
+++ relay/starter/starter.go
@@ -1,11 +1,11 @@
-package main
+package starter
 
 import (
 	"github.com/tcdt-lab/Automated-Gateways/relay/internal/offchain/server"
 	cli "github.com/tcdt-lab/Automated-Gateways/relay/iop_cli"
 )
 
-func startIop() {
+func StartIop() {
 	go server.StartServer()
 	cli.StartCli()
 }
