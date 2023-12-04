c921b83615a77e12603687d5a48bc7e75533d615
koosha <k.e.khorasani@gmail.com>
2023-12-03 19:32:21 -0600

    starter is added
    Signed-off-by: koosha <k.e.khorasani@gmail.com>

--- relay/cmd/main.go
+++ relay/cmd/main.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"github.com/tcdt-lab/Automated-Gateways/relay/configs"
+	"github.com/tcdt-lab/Automated-Gateways/relay/starter"
 )
 
 func main() {
 	configs.YamlAddress = "/home/koosha/Desktop/Automated-Gateways/relay/cmd/config.yaml"
-	startIop()
+	starter.StartIop()
 }
