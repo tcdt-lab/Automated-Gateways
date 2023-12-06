3c2eb0dfdcb80184d3c2a4bd70c426639996b528
koosha <k.e.khorasani@gmail.com>
2023-12-06 09:29:15 -0600

    Minor changes have been applied

--- relay/iop_cli/CliFsmHandler.go
+++ relay/iop_cli/CliFsmHandler.go
@@ -177,19 +177,19 @@ func (fsm *StateMachine) FsmCreator() *StateMachine {
 			STATE_SELECTING_INVOKE_METHOD: {
 				EVENT_RETURN_TO_PREVIOUS_MENU: {
 					actionFunction: showOutsideDataOption,
-					destination:    STATE_SELECTING_OUTSIDE_DATA,
+					destination:    STATE_SELECTING_OUTSIDE_DATA_OPTIONS,
 				},
 			},
 			STATE_SELECTING_GET_ACCESSIBLE_NETWORK_INFO: {
 				EVENT_RETURN_TO_PREVIOUS_MENU: {
 					actionFunction: showOutsideDataOption,
-					destination:    STATE_SELECTING_OUTSIDE_DATA,
+					destination:    STATE_SELECTING_OUTSIDE_DATA_OPTIONS,
 				},
 			},
 			STATE_SELECTING_GET_ACCESSIBLE_METHOD_LIST: {
 				EVENT_RETURN_TO_PREVIOUS_MENU: {
 					actionFunction: showOutsideDataOption,
-					destination:    STATE_SELECTING_OUTSIDE_DATA,
+					destination:    STATE_SELECTING_OUTSIDE_DATA_OPTIONS,
 				},
 			},
 			//*********************** INSIDE DATA STATES *************************
