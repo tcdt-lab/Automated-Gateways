6fd501d5f267cb38271dcb3c37b952ac34d9e5aa
koosha <k.e.khorasani@gmail.com>
2024-02-22 11:55:35 -0600

    Changing the starter
    Thread handeling will be done by user not library
    
    Signed-off-by: koosha <k.e.khorasani@gmail.com>

--- relay/cmd/main.go
+++ relay/cmd/main.go
@@ -2,10 +2,38 @@ package main
 
 import (
 	"github.com/tcdt-lab/Automated-Gateways/relay/configs"
+	"github.com/tcdt-lab/Automated-Gateways/relay/mediator"
 	"github.com/tcdt-lab/Automated-Gateways/relay/starter"
+	"sync"
 )
 
 func main() {
+	var wg sync.WaitGroup
 	configs.YamlAddress = "/home/koosha/Desktop/Automated-Gateways/relay/cmd/config.yaml"
-	starter.StartIop()
+
+	go func() {
+		wg.Add(1)
+		defer wg.Done()
+		starter.StartIopServer()
+	}()
+
+	go func() {
+		wg.Add(1)
+		defer wg.Done()
+		starter.StartIopCLI()
+	}()
+
+	var mediatorFactory mediator.MediatorFactory
+
+	permittedNetwork, err := mediatorFactory.CreatePermittedNetworkMediator(mediator.HYPERLEDGER_FABRIC_NETWROK_TYPE)
+	if err != nil {
+		panic(err)
+	}
+	permittedNt, err := permittedNetwork.GetAllPermittedNetworksByAddress("localhost")
+
+	for _, v := range permittedNt {
+		println(v.NetworkName)
+	}
+	wg.Wait()
+
 }
