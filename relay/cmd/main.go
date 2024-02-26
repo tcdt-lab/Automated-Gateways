fcede1cdfb3bd958c4122beb37321738437b2268
koosha <k.e.khorasani@gmail.com>
2024-02-26 11:17:57 -0600

    Main Sample
    we have a sample in main.go for initial running of CLI and gRPC server
    
    Signed-off-by: koosha <k.e.khorasani@gmail.com>

--- relay/cmd/main.go
+++ relay/cmd/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"github.com/tcdt-lab/Automated-Gateways/relay/configs"
-	"github.com/tcdt-lab/Automated-Gateways/relay/mediator"
 	"github.com/tcdt-lab/Automated-Gateways/relay/starter"
 	"sync"
 )
@@ -10,30 +9,19 @@ import (
 func main() {
 	var wg sync.WaitGroup
 	configs.YamlAddress = "/home/koosha/Desktop/Automated-Gateways/relay/cmd/config.yaml"
-
+	wg.Add(1)
 	go func() {
-		wg.Add(1)
+
 		defer wg.Done()
 		starter.StartIopServer()
 	}()
-
+	wg.Add(1)
 	go func() {
-		wg.Add(1)
+
 		defer wg.Done()
 		starter.StartIopCLI()
 	}()
 
-	var mediatorFactory mediator.MediatorFactory
-
-	permittedNetwork, err := mediatorFactory.CreatePermittedNetworkMediator(mediator.HYPERLEDGER_FABRIC_NETWROK_TYPE)
-	if err != nil {
-		panic(err)
-	}
-	permittedNt, err := permittedNetwork.GetAllPermittedNetworksByAddress("localhost")
-
-	for _, v := range permittedNt {
-		println(v.NetworkName)
-	}
 	wg.Wait()
 
 }
